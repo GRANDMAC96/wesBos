1d78adc5de3cfe2e92f52fb050ea3fef8e94a16b
Henry Macfarlane <hjemacfarlane@gmail.com>
2021-05-31 17:55:16 +0100

    set proptypes of fish

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -1,8 +1,20 @@
 import React from "react";
-import PropTypes from "prop-types";
+import PropTypes, { shape } from "prop-types";
 import { formatPrice } from "../helpers";
 
 class Fish extends React.Component {
+    // There is more documentation about proptypes online on react's website
+    // Here shape is a function that accepts an object where we can specify what goes inside it.
+    static propTypes = {
+        details: shape({
+            image: PropTypes.string,
+            name: PropTypes.string,
+            price: PropTypes.number,
+            status: PropTypes.string,
+            desc: PropTypes.string,
+        }),
+        addToOrder: PropTypes.func,
+    }
     // Can use a handleClick method below with the button but often theres no need to if the function only does one thing.
     // Therefore I just wrote the function inline
     // handleClick = () => {
