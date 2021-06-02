7df96d85e15bf142eedc2b9c6d61812d17948d00
Henry Macfarlane <hjemacfarlane@gmail.com>
2021-06-02 17:25:37 +0100

    adjusted login.js to use authenticate via props

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,19 @@
 import React from "react";
 
-const Login = () => (
+const Login = (props) => (
     <nav className="login">
         <p>Sign in to manage your store's Inventory</p>
         <button
             className="github"
-            onClick={() => this.props.authenticate("Github")}
+            onClick={() => props.authenticate("Github")}
         >Log in with Github</button>
         <button
             className="twitter"
-            onClick={() => this.props.authenticate("Twitter")}
+            onClick={() => props.authenticate("Twitter")}
         >Log in with Twitter</button>
         <button
             className="facebook"
-            onClick={() => this.props.authenticate("Facebook")}
+            onClick={() => props.authenticate("Facebook")}
         >Log in with Facebook</button>
     </nav>
 )
