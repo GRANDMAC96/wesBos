36a14c9dfebd588227c01d4337d9dc1f789ff4b7
Henry Macfarlane <hjemacfarlane@gmail.com>
2021-06-03 18:35:29 +0100

    build folder added

---
+++ build/precache-manifest.e941db681fedae0cae622782374ee723.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9496569a299ce9f84a325f056c4ae37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b21cf3a8ab6b83c2a555",
+    "url": "/static/css/main.3a04dc70.chunk.css"
+  },
+  {
+    "revision": "a9e72088d507d7f8de15",
+    "url": "/static/js/2.37fb2553.chunk.js"
+  },
+  {
+    "revision": "9b50f5741be789e4838182ccc7c0c6a5",
+    "url": "/static/js/2.37fb2553.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b21cf3a8ab6b83c2a555",
+    "url": "/static/js/main.096dedf7.chunk.js"
+  },
+  {
+    "revision": "682f9dc0a88c8bde3947",
+    "url": "/static/js/runtime-main.eb0b6bae.js"
+  },
+  {
+    "revision": "d2f8799a13e6db2022d9a7de22776705",
+    "url": "/static/media/anchor.d2f8799a.svg"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "7bbffda6bcacac8360f0ee00bb03dc04",
+    "url": "/static/media/blanch_caps_inline-webfont.7bbffda6.svg"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "cf798e30eda73d84b25c4ae233fe5e21",
+    "url": "/static/media/haymaker-webfont.cf798e30.svg"
+  },
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/static/media/haymaker-webfont.e9535f20.ttf"
+  }
+]);
